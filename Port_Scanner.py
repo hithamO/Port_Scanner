9f1d3c51511b2f95734973efebbb81238e2d9912
Hitham Attallah <97394784+hithamO@users.noreply.github.com>
2024-05-25 21:28:28 +0300

    Update Port_Scanner.py

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -2,37 +2,36 @@ import sys
 import socket
 from datetime import datetime
 
-# Define our target
+#Define our target
 if len(sys.argv) == 2:
-    target = socket.gethostbyname(sys.argv[1])
+	target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
 else:
-    print("Invalid amount of arguments")
-    print("Syntax: python3 scanner.py <ip>")
-    sys.exit()
+	print("Invalid amount of arguments.")
+	print("Syntax: python3 scanner.py")
 
-# Add a pretty banner
+#Add a pretty banner
 print("-" * 50)
-print("Scanning target "+ target)
+print("Scanning target "+target)
 print("Time started: "+str(datetime.now()))
 print("-" * 50)
 
 try:
-    for port in range(50, 85):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-        result = s.connect_ex((target, port))
-        if result == 0:
-            print(f"Port {port} is open.")
-        s.close()
+	for port in range(50,85):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target,port)) #returns an error indicator - if port is open it throws a 0, otherwise 1
+		if result == 0:
+			print("Port {} is open".format(port))
+		s.close()
 
 except KeyboardInterrupt:
-    print("\nExiting program.")
-    sys.exit()
-
+	print("\nExiting program.")
+	sys.exit()
+	
 except socket.gaierror:
-    print("Hostname could not be resolved.")
-    sys.exit()
+	print("Hostname could not be resolved.")
+	sys.exit()
 
 except socket.error:
-    print("Could not connect to server.")
-    sys.exit()
+	print("Could not connect to server.")
+	sys.exit()
